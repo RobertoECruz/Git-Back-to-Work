b0b8c74474e499666c15027c539ea434f4f482c1
Roberto Cruz <RobertoECruz12@gmail.com>
2013-09-10 16:43:03 -0400

    learned what makes node faster than python...kernels...got rid of python/Flask replaced it with express

--- gbtw/app.js
+++ gbtw/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
@@ -27,12 +28,33 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(request, respond) {
+  fs.readFile('index.html', function(err, data){
+    respond.end(data);
+  });
+});
 
-app.post('/', function(request, response){
-  console.log(request.body.user.name);
-  console.log(request.body.user.email);
+app.get('/users', user.list);
+//write post data to users.txt file 
+
+app.post('/receive', function(request, respond) {
+  var filePath = __dirname + '/users.txt';
+  var name = request.body.name;
+  var email = request.body.email;
+//this is an anonymous function
+//it can access all of the information in app.post(outside of it?)
+//because javascript has a feature called lexical feature
+//they depend on where that function is defined
+  var appendFileCallback = function (err) {
+    if (err) {
+      respond.end("There was an error: " + err);
+    } else {
+      respond.end("Thank you for submitting your username and email.");
+    }
+  };
+//fs is a library that comes with node, appendFileCallback is an anonymous function
+//fs.appendfile gets run first, because it is not an anonymous function
+  fs.appendFile(filePath, name + " " + email + "\n", appendFileCallback);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
